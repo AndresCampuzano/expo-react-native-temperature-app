75023526d33e8e991d64df80552b49a525934d93
Andres Campuzano <campuxano88@gmail.com>
2025-04-17 16:10:40 -0500

    initialize project with Tailwind CSS and NativeWind setup

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View>
+      <Text className={'text-red-800 bg-red-200 font-bold text-center py-5'}>
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
