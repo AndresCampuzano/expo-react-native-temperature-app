d897dd5bca0c0592b61b9b0bd4380d1bdd0707d9
Andres Campuzano <campuxano88@gmail.com>
2025-04-18 16:48:22 -0500

    add temp and location elements in UI

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,20 @@
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import './globals.css';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <StatusBar style="dark" />
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#F0ECC6' }}>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: { backgroundColor: '#F0ECC6' },
+          }}
+        />
+      </SafeAreaView>
+    </>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,40 @@
 import { Text, View } from 'react-native';
+import { MapPinIcon } from 'react-native-heroicons/outline';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Index() {
   return (
-    <View>
-      <Text className={'text-red-800 bg-red-200 font-bold text-center py-5'}>
-        Edit app/index.tsx to edit this screen.
-      </Text>
+    <View className={'flex-1'}>
+      <Text className={'w-full text-center mt-9 text-2xl'}>Today</Text>
+      <View className={'relative flex justify-center flex-col'}>
+        {/* Sun (Yellow Gradient Circle) */}
+        <LinearGradient
+          colors={['#F8BE28', 'rgba(248, 190, 40, 0)']}
+          style={{
+            position: 'absolute',
+            width: '75%',
+            aspectRatio: 1,
+            borderRadius: 9999,
+            alignSelf: 'center',
+            zIndex: -1,
+          }}
+        />
+        {/* Main Content */}
+        <View className={'flex flex-col justify-center items-center text-center'}>
+          <View className={'my-3 flex flex-row'}>
+            <View>
+              <Text className={'text-[200px] font-extralight'}>28</Text>
+            </View>
+            <View>
+              <Text className={'text-4xl font-light mt-[50px]'}>°C</Text>
+            </View>
+          </View>
+          <View className={'flex flex-col items-center justify-center'}>
+            <MapPinIcon color="black" size={32} />
+            <Text className={'text-[170px] font-thin'}>BOG</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
